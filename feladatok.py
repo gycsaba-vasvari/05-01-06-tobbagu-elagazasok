dfc29666759dd8772923a5c5dab2f26add6cd561
Gyuris Csaba <gycsaba72@gmail.com>
2022-02-23 11:26:27 +0100

    Update feladatok.py

--- feladatok.py
+++ feladatok.py
@@ -3,7 +3,7 @@
 # 1. feladat
 # Készítsen függvényt viz_allapot néven
 # A függvény bemeneti értéke a víz hőmérséklete
-# A függvény a pára, folyékony és jég szavakat adja vissza a hőmérsékletnek megfelelően
+# A függvény a pára, víz és jég szavakat adja vissza a hőmérsékletnek megfelelően
 
 # 2. feladat
 # Készítsen függvényt signum néven
@@ -16,7 +16,7 @@
 # A kimenet a jeles, jó, közepes, elégséges,  elégtelen, és értelmezhetetlen szavak a jegytől függően
 
 # 4. feladat
-# Készítsen függvényt koorinatarendszer néven
+# Készítsen függvényt koordinatarendszer néven
 # Bemeneti paramétere az x és y koorináta
 # A függvény az "x. negyed" (x=1..4) vagy "x tengely" vagy "y tengely" "vagy "origó" értékkel térjen vissza
 
